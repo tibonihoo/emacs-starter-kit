ab2e3f03c757a5050ad068a783d095efd4445f09
Thibauld <nion@work>
2010-11-16 10:22:44 +0100

    add helper function to surround a word or region with a tag

--- starter-kit-defuns.el
+++ starter-kit-defuns.el
@@ -139,6 +139,28 @@ Symbols matching the text at point are put first in the completion list."
                                     ,(make-char 'greek-iso8859-7 107))
                     nil))))))
 
+;; XML tags everywhere
+;; from
+;; http://www.johndcook.com/blog/2010/08/05/emacs-command-to-add-html-tags/
+
+(defun tag-word-or-region (tag)
+    "Surround current word or region with a given tag."
+    (interactive "sEnter tag (without <>): ")
+    (let (pos1 pos2 bds start-tag end-tag)
+        (setq start-tag (concat "<" tag ">"))
+        (setq end-tag (concat "</" tag ">"))
+        (if (and transient-mark-mode mark-active)
+            (progn
+                (goto-char (region-end))
+                (insert end-tag)
+                (goto-char (region-beginning))
+                (insert start-tag))
+            (progn
+                (setq bds (bounds-of-thing-at-point 'symbol))
+                (goto-char (cdr bds))
+                (insert end-tag)
+                 (goto-char (car bds))
+                 (insert start-tag)))))
 
 
 ;; Other
