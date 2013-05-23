55ad3f6d862b442399ff98da6ddb539bf0158b19
Thibauld Nion <thibauld@tibonihoo.net>
2013-05-23 15:36:28 +0200

    add melpa to repo for package.el

--- init.el
+++ init.el
@@ -55,6 +55,9 @@
 (add-to-list 'package-archives 
              '("marmalade" .
                "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives 
+             '("melpa" .
+               "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 (require 'starter-kit-elpa)
 
