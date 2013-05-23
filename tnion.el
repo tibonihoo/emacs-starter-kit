6c2ea4669702ad5dfb17ba652e0cacc33ee8271f
Thibauld Nion <thibauld@tibonihoo.net>
2013-05-23 15:41:33 +0200

    configure log4j and cedet's semantic for C++ cmake projects

--- tnion.el
+++ tnion.el
@@ -35,6 +35,16 @@
   (set-face-attribute 'default nil :family "b&h-lucidatypewriter")
   )
 
+;; read the log of DOP
+(autoload 'log4j-mode "log4j-mode" "Major mode for viewing log files." t)
+(add-to-list 'auto-mode-alist '("DxOOpticsPro\\.[0-9]*\\.txt\\'" . log4j-mode))
+(add-hook
+ 'log4j-mode-hook
+ (lambda ()
+   (define-key log4j-mode-local-map [(control down)] 'log4j-forward-record)
+   (define-key log4j-mode-local-map [(control up)] 'log4j-backward-record)
+   (define-key log4j-mode-local-map [(control c) (control f)] 'font-lock-fontify-buffer)
+   ))
 
 (add-hook 'c-mode-common-hook
           '(lambda () 
@@ -89,7 +99,7 @@
              ;; (semantic-idle-scheduler-mode)
              ;; (add-to-list 'semanticdb-project-roots "d:/DATA/WORK/Framework-trunk")
              ;; (add-to-list 'semanticdb-project-roots "d:/DATA/WORK/Framework-2-x")
-             ;; (add-to-list 'ac-sources 'ac-source-semantic)
+             (add-to-list 'ac-sources 'ac-source-gtags)
              (set-compilation-regexp-alist-for-msbuild)
              ;; trigger other common coding hooks
              ))
@@ -97,9 +107,180 @@
 
 (setup-cmake-mode)
 
-(setq  compile-command "cmake ../.. && msbuild /p:configuration=release /m:8 Framework.sln && ctest -C release --output-on-failure")
+(setq  compile-command "cmake -G \"Visual Studio 10 Win64\" -DUTTestImagesPathNetwork=D:/DATA/UTData ../.. && msbuild /p:configuration=release /m:8 Framework.sln && ctest -C release --output-on-failure")
 (setq  fill-column 110)
-(setq grep-command "grep -nH --exclude-dir=\\.svn --exclude-dir=build -r ")
+(setq grep-command "grep -nH --exclude-dir=\\.svn --exclude-dir=build --exclude-dir=Externals -r ")
+
+
+
+;; -----------------------------------------------------------------------------
+;; Semantic config
+;; -----------------------------------------------------------------------------
+;; Partly from https://github.com/alexott/emacs-configs/blob/master/rc/emacs-rc-cedet.el
+
+(add-to-list 'semantic-default-submodes 'global-semanticdb-minor-mode)
+;(add-to-list 'semantic-default-submodes 'global-semantic-mru-bookmark-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-scheduler-mode)
+(add-to-list 'semantic-default-submodes 'global-semanticdb-minor-mode)
+;(add-to-list 'semantic-default-submodes 'global-semantic-stickyfunc-mode)
+;; Activate semantic
+(semantic-mode 1)
+(add-to-list 'ac-sources 'ac-source-semantic)
+;; ;; use GLOBAL tag db
+;; (semanticdb-enable-gnu-global-databases 'c-mode t)
+(semanticdb-enable-gnu-global-databases 'c++-mode)
+
+
+;; EDE
+(require 'ede)
+(global-ede-mode 1)
+(ede-enable-generic-projects)
+
+(defvar ede-project-build-current-build-platform "win64")
+(defvar ede-project-build-current-build-mode "release")
+(defvar ede-project-build-current-test-options "")
+
+(defun generate-framework-cmake-build-command ()  
+  "Generates compile string for compiling CMake project"
+  (let* (
+         (current-dir (file-name-directory
+                       (or (buffer-file-name (current-buffer)) default-directory)))
+         (prj (ede-current-project current-dir))
+         (root-dir (ede-project-root-directory prj))
+         (cmake-generator
+          (cond
+           ( (string= ede-project-build-current-build-platform "win64")
+             "-G \"Visual Studio 10 Win64\""
+             )
+           ( t
+             "")
+           ))
+         (build-dir-name
+          (cond
+           ( (string= ede-project-build-current-build-platform "win64")
+             "visualx64"
+             )
+           ( t
+             "visualx86")
+           ))
+         )
+    (concat "cd " root-dir "/build/ && mkdir " build-dir-name " || cd " build-dir-name " && cmake ../.. " cmake-generator " -DUTTestImagesPathNetwork=D:/DATA/UTData && msbuild /p:configuration=" ede-project-build-current-build-mode " /m:8 Framework.sln")
+    )
+  )
+
+(defun generate-framework-cmake-build-and-test-command ()
+  "Generates compile string for compiling CMake project and running unit-test on it"
+  (concat (generate-framework-cmake-build-command) " && ctest -C " ede-project-build-current-build-mode " --output-on-failure " ede-project-build-current-test-options)
+  )
+
+(ede-cpp-root-project "Framework-trunk"
+                      :name "Framework-trunk"
+                :file "d:/DATA/WORK/Framework-trunk/CMakeLists.txt"
+                :include-path '("/include"
+                                "/src/Correction/include"
+                                "/src/Foundation/include"
+                                )
+                :local-variables (list
+                                  (cons 'compile-command 'generate-framework-cmake-build-command)
+                                  (cons 'compile-and-test-command 'generate-framework-cmake-build-and-test-command))
+                :system-include-path '("c:/Program Files (x86)/Microsoft Visual Studio 10.0/VC/include")
+                :spp-table '(("WIN32" . "")
+                             ("BOOST_TEST_DYN_LINK" . "")))
+
+
+
+(ede-cpp-root-project "Framework-3.5.x"
+                      :name "Framework-3.5.x"
+                      :file "d:/DATA/WORK/Framework-3.5.x/CMakeLists.txt"
+                      :include-path '("/include"
+                                      "/src/Correction/include"
+                                      "/src/Foundation/include"
+                                      )
+                      :local-variables (list
+                                        (cons 'compile-command 'generate-framework-cmake-build-command)
+                                        (cons 'compile-and-test-command 'generate-framework-cmake-build-and-test-command))
+                      :system-include-path '("c:/Program Files (x86)/Microsoft Visual Studio 10.0/VC/include")
+                      :spp-table '(("WIN32" . "")
+                                   ("BOOST_TEST_DYN_LINK" . "")))
+
+
+(ede-cpp-root-project "Framework-3.x"
+                      :name "Framework-3.x"
+                      :file "d:/DATA/WORK/Framework-3.x/CMakeLists.txt"
+                      :include-path '("/include"
+                                      "/src/Correction/include"
+                                      "/src/Foundation/include"
+                                      )
+                      :local-variables (list
+                                        (cons 'compile-command 'generate-framework-cmake-build-command)
+                                        (cons 'compile-and-test-command 'generate-framework-cmake-build-and-test-command))
+                      :system-include-path '("c:/Program Files (x86)/Microsoft Visual Studio 10.0/VC/include")
+                      :spp-table '(("WIN32" . "")
+                                   ("BOOST_TEST_DYN_LINK" . "")))
+
+
+;; from https://github.com/alexott/emacs-configs/blob/master/rc/emacs-rc-cedet.el
+(defun ede-get-local-var (fname var)
+  "fetch given variable var from :local-variables of project of file fname"
+  (let* ((current-dir (file-name-directory fname))
+         (prj (ede-current-project current-dir)))
+    (when prj
+      (let* ((ov (oref prj local-variables))
+            (lst (assoc var ov)))
+        (when lst
+          (cdr lst))))))
+
+(defun build-ede-project (&optional build-target-platform build-target-mode)
+  "Saves all unsaved buffers, and compile the current project."
+  (interactive)
+  (save-some-buffers t)
+  (when (not build-target-platform)
+    (setq build-target-platform
+          (read-string "platform: " ede-project-build-current-build-platform))
+    )
+  (when (not build-target-mode)
+    (setq build-target-mode
+          (read-string "mode: " ede-project-build-current-build-mode))
+    )
+  (setq ede-project-build-current-build-platform build-target-platform)
+  (setq ede-project-build-current-build-mode build-target-mode)
+  (let* ((r (ede-get-local-var
+             (or (buffer-file-name (current-buffer)) default-directory)
+             'compile-command))
+         (cmd (if (functionp r) (funcall r) r)))
+    (set (make-local-variable 'compile-command) (or cmd compile-command))
+    (compile compile-command))
+  )
+
+(defun test-ede-project (&optional build-target-platform build-target-mode build-target-test-options)
+  "Saves all unsaved buffers, and compile the current project."
+  (interactive)
+  (save-some-buffers t)
+  (when (not build-target-platform)
+    (setq build-target-platform
+          (read-string "platform: " ede-project-build-current-build-platform))
+    )
+  (when (not build-target-mode)
+    (setq build-target-mode
+          (read-string "mode: " ede-project-build-current-build-mode))
+    )
+  (when (not build-target-test-options)
+    (setq build-target-test-options
+          (read-string "test-options: " ede-project-build-current-test-options))
+    )
+  (setq ede-project-build-current-build-platform build-target-platform)
+  (setq ede-project-build-current-build-mode build-target-mode)
+  (setq ede-project-build-current-test-options build-target-test-options)
+  (let* ((r (ede-get-local-var
+             (or (buffer-file-name (current-buffer)) default-directory)
+             'compile-and-test-command))
+         (cmd (if (functionp r) (funcall r) r)))
+    (set (make-local-variable 'compile-command) (or cmd compile-command))
+    (compile compile-command))
+  )
+
+
+
 
 ;; (custom-set-variables
 ;;  ;; custom-set-variables was added by Custom.
