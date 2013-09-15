34df55a74f4221c04a76ac787b712cb4407b98df
tibonihoo <thibauld@tibonihoo.net>
2013-09-15 12:26:14 +0200

    de-activate a little too lispy awkward js mode

--- starter-kit-js.el
+++ starter-kit-js.el
@@ -5,8 +5,8 @@
 (autoload 'espresso-mode "espresso" "Start espresso-mode" t)
 (add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
-(add-hook 'espresso-mode-hook 'moz-minor-mode)
-(add-hook 'espresso-mode-hook 'esk-paredit-nonlisp)
+;; (add-hook 'espresso-mode-hook 'moz-minor-mode)
+;; (add-hook 'espresso-mode-hook 'esk-paredit-nonlisp)
 (add-hook 'espresso-mode-hook 'run-coding-hook)
 (add-hook 'espresso-mode-hook 'idle-highlight)
 (setq espresso-indent-level 2)
@@ -22,7 +22,7 @@
           (font-lock-add-keywords
            'espresso-mode `(("\\(function *\\)("
                              (0 (progn (compose-region (match-beginning 1)
-                                                       (match-end 1) "ƒ")
+                                                       (match-end 1) "λ")
                                        nil)))))))
 
 (provide 'starter-kit-js)
