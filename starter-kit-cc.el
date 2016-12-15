f2484fc657bfd10ca18f5b58e7a7920562438757
tibonihoo <thibauld@tibonihoo.net>
2016-12-15 16:14:16 +0100

    change end of lines + remove a useless binding for vs/git add

--- starter-kit-cc.el
+++ starter-kit-cc.el
@@ -40,8 +40,8 @@
      ;; navigation between corresponding header and source files
      (require 'sourcepair)
      (setq sourcepair-recurse-ignore  (append sourcepair-recurse-ignore '(".svn" ".hg" ".git" )))
-     (setq sourcepair-source-path  (append sourcepair-source-path '("../src" "../source" "./src" "./source" "../*" "../../src/*")))
-     (setq sourcepair-header-path  (append sourcepair-header-path '("../include" "../inc" "../*" "../../include/*")))
+     (setq sourcepair-source-path  (append sourcepair-source-path '("../../src/*" "../src" "../source" "./src" "./source" "../*")))
+     (setq sourcepair-header-path  (append sourcepair-header-path '("../../include/*" "../include" "../inc" "../*")))
      (define-key c-mode-base-map (kbd "C-c o") 'sourcepair-load)
      ;; Doxygen help (but don't fail if doxymacs cannot be found)
      (condition-case err
@@ -125,7 +125,7 @@
            (local-file (file-relative-name
                         temp-file
                         (file-name-directory buffer-file-name)))
-           (option-list (add-clang-include-options-for-ede-project (list "-fsyntax-only" "-fno-color-diagnostics" "-x" "c++" local-file)))
+           (option-list (add-clang-include-options-for-ede-project (list "-Wall" "-Werror" "-fsyntax-only" "-fno-color-diagnostics" "-x" "c++" "-std=c++14"  "-DDEFAULT_IP_IMPL=ip_ipp" local-file)))
            )
       ;;(message option-list)
       (list "clang++" option-list)
@@ -155,6 +155,78 @@
 	     (setq mode-name "C++")
 	     )
 	  );; end of c++-mode-hook
+
+
+;; Fontify C++11+ novelties
+;; http://stackoverflow.com/questions/8549351/c11-mode-or-settings-for-emacs
+(add-hook
+ 'c++-mode-hook
+ '(lambda()
+    ;; We could place some regexes into `c-mode-common-hook', but note that their evaluation order
+    ;; matters.
+    (font-lock-add-keywords
+     nil '(;; complete some fundamental keywords
+           ("\\<\\(void\\|unsigned\\|signed\\|char\\|short\\|bool\\|int\\|long\\|float\\|double\\)\\>" . font-lock-keyword-face)
+           ;; namespace names and tags - these are rendered as constants by cc-mode
+           ("\\<\\(\\w+::\\)" . font-lock-function-name-face)
+           ;;  new C++11 keywords
+           ("\\<\\(alignof\\|alignas\\|constexpr\\|decltype\\|noexcept\\|nullptr\\|static_assert\\|thread_local\\|override\\|final\\)\\>" . font-lock-keyword-face)
+           ("\\<\\(char16_t\\|char32_t\\)\\>" . font-lock-keyword-face)
+           ;; PREPROCESSOR_CONSTANT, PREPROCESSORCONSTANT
+           ("\\<[A-Z]*_[A-Z_]+\\>" . font-lock-constant-face)
+           ("\\<[A-Z]\\{3,\\}\\>"  . font-lock-constant-face)
+           ;; hexadecimal numbers
+           ("\\<0[xX][0-9A-Fa-f]+\\>" . font-lock-constant-face)
+           ;; integer/float/scientific numbers
+           ("\\<[\\-+]*[0-9]*\\.?[0-9]+\\([ulUL]+\\|[eE][\\-+]?[0-9]+\\)?\\>" . font-lock-constant-face)
+           ;; c++11 string literals
+           ;;       L"wide string"
+           ;;       L"wide string with UNICODE codepoint: \u2018"
+           ;;       u8"UTF-8 string", u"UTF-16 string", U"UTF-32 string"
+           ("\\<\\([LuU8]+\\)\".*?\"" 1 font-lock-keyword-face)
+           ;;       R"(user-defined literal)"
+           ;;       R"( a "quot'd" string )"
+           ;;       R"delimiter(The String Data" )delimiter"
+           ;;       R"delimiter((a-z))delimiter" is equivalent to "(a-z)"
+           ("\\(\\<[uU8]*R\"[^\\s-\\\\()]\\{0,16\\}(\\)" 1 font-lock-keyword-face t) ; start delimiter
+           (   "\\<[uU8]*R\"[^\\s-\\\\()]\\{0,16\\}(\\(.*?\\))[^\\s-\\\\()]\\{0,16\\}\"" 1 font-lock-string-face t)  ; actual string
+           (   "\\<[uU8]*R\"[^\\s-\\\\()]\\{0,16\\}(.*?\\()[^\\s-\\\\()]\\{0,16\\}\"\\)" 1 font-lock-keyword-face t) ; end delimiter
+
+           ;; user-defined types (rather project-specific)
+           ("\\<[A-Za-z_]+[A-Za-z_0-9]*_\\(type\\|ptr\\)\\>" . font-lock-type-face)
+           ("\\<\\(xstring\\|xchar\\)\\>" . font-lock-type-face)
+           ))
+    ) t)
+
+
+;; Indent enum class correctly
+;; taken from http://stackoverflow.com/questions/6497374/emacs-cc-mode-indentation-problem-with-c0x-enum-class
+(defun inside-class-enum-p (pos)
+  "Checks if POS is within the braces of a C++ \"enum class\"."
+  (ignore-errors
+    (save-excursion
+      (goto-char pos)
+      (up-list -1)
+      (backward-sexp 1)
+      (looking-back "enum[ \t]+class[ \t]+[^}]+"))))
+
+(defun align-enum-class (langelem)
+  (if (inside-class-enum-p (c-langelem-pos langelem))
+      0
+    (c-lineup-topmost-intro-cont langelem)))
+
+(defun align-enum-class-closing-brace (langelem)
+  (if (inside-class-enum-p (c-langelem-pos langelem))
+      '-
+    '+))
+
+(defun fix-enum-class ()
+  "Setup `c++-mode' to better handle \"class enum\"."
+  (add-to-list 'c-offsets-alist '(topmost-intro-cont . align-enum-class))
+  (add-to-list 'c-offsets-alist
+               '(statement-cont . align-enum-class-closing-brace)))
+
+(add-hook 'c++-mode-hook 'fix-enum-class)
 
 
 (provide 'starter-kit-cc)
