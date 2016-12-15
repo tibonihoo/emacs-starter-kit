bbd913ecae935be214bc3e0fe1a4a5ef290187d6
tibonihoo <thibauld@tibonihoo.net>
2016-12-15 16:15:10 +0100

    fix font and remove useless stuff

--- thibauld.el
+++ thibauld.el
@@ -21,7 +21,7 @@
    (set-face-attribute 'default nil :family "Menlo")
    )
  ( t
-   (set-face-attribute 'default nil :family "Monospace" :height 105)
+   (set-face-attribute 'default nil :family "Monospace")
   )
  )
 
@@ -42,6 +42,8 @@
 (delete-selection-mode t)
 ;; Tab navigation among buffers
 (require 'ibs)
+
+
 
 
 ;; -----------------------------------------------------------------------------
@@ -85,6 +87,8 @@
      compilation-finish-functions
      )
 
+
+
 ;; -----------------------------------------------------------------------------
 ;; Dictionary definitions for ispell
 ;; -----------------------------------------------------------------------------
@@ -110,6 +114,8 @@
 ;; mispelled word (default) M-o correct current word (instead of
 ;; M-TAB)
 (global-set-key "\M-o" 'flyspell-auto-correct-word)
+
+
 
 
 ;; -----------------------------------------------------------------------------
@@ -198,6 +204,8 @@
       (count-matches target-symbol))))
 
 (global-set-key [(meta m)] 'count-symbol-at-point)
+
+
 
 
 ;; -----------------------------------------------------------------------------
@@ -291,6 +299,7 @@
 
 
 
+
 ;; -----------------------------------------------------------------------------
 ;; Misc customisations
 ;; -----------------------------------------------------------------------------
@@ -306,6 +315,7 @@
              (local-set-key (kbd "M-n") 'flymake-goto-next-error)
              (local-set-key (kbd "M-p") 'flymake-goto-prev-error)
              ))
+
 
 
 ;; -----------------------------------------------------------------------------
@@ -348,6 +358,7 @@
      ))
 
 
+
 ;; -----------------------------------------------------------------------------
 ;; C/C++ personal config
 ;;-----------------------------------------------------------------------------
@@ -383,6 +394,7 @@
              ))
 
 
+
 ;; -----------------------------------------------------------------------------
 ;; LaTeX personal config
 ;; -----------------------------------------------------------------------------
@@ -401,6 +413,7 @@
       ;;       (quote (".//" "..//")))
       ))
   )
+
 
 
 
@@ -441,6 +454,8 @@ select the source buffer."
   (define-key	grep-mode-map "A" 'first-error)
   )
 (add-hook 'grep-setup-hook 'my-grep-hook)
+
+
 
 
 ;; -----------------------------------------------------------------------------
@@ -522,6 +537,8 @@ select the source buffer."
 (global-set-key [(control ?:)] 'duplicate-line)
 
 
+
+
 ;; -----------------------------------------------------------------------------
 ;; Buffer stats
 ;; -----------------------------------------------------------------------------
@@ -537,6 +554,8 @@ select the source buffer."
       (count-matches target-symbol))))
 
 (global-set-key [(meta m)] 'count-symbol-at-point)
+
+
 
 
 ;; -----------------------------------------------------------------------------
@@ -607,22 +626,6 @@ select the source buffer."
   )
 
 
-(defun directory-parent-directory (directory-name)
-  "Return the parent directory of a given directory"
-  (file-name-directory 
-   (directory-file-name 
-    (expand-file-name directory-name)
-    )
-   )
-  )
-
-(defun directory-is-root (directory-name)
-  "Return t if the directory name is a root of the filesystem (that is '/' or 'c:/', 'd:/' (...) on Windows."
-  (string= 
-   (directory-parent-directory directory-name)
-   directory-name
-   )
-  )
 
 (defun directory-parent-directory (directory-name)
   "Return the parent directory of a given directory"
@@ -695,56 +698,6 @@ select the source buffer."
   )
 
 
-
-(defun generate-doc-doxygen ()
-  "Generate Doxygen documentation related to current file or folder"
-  (interactive)
-  (let  ( 
-	 ;; the directory where the doxygen.cfg file should be located (if any)
-	 doxygen-config-parent-directory 
-	 ;; the full path to the doxygen.cfg file (if it exists)
-	 doxygen-config-file
-	 ;; "expected" full path to the index of the generated doc
-	 doxygen-html-index-file
-	 ;; remember in which directory the function is run
-	 (saved-default-directory default-directory)
-	 )
-    ;; lookup for the directory containing the closest doxygen.conf
-    ;; the file hierarchy).
-    (setq doxygen-config-parent-directory
-	  (locate-dominating-file 
-	   (directory-file-name (expand-file-name default-directory))
-	   "doxygen.cfg"
-	   )
-	  )
-    (if doxygen-config-parent-directory
-	  (progn
-	    (setq doxygen-config-parent-directory (expand-file-name doxygen-config-parent-directory))
-	    (setq doxygen-config-file (concat doxygen-config-parent-directory "/doxygen.cfg"))
-	    (setq doxygen-html-index-file (concat doxygen-config-parent-directory "/build_doc/html/index.html"))
-	    ;; bullet proofize against windows "\" vs "/" plague
-	    (if (eq system-type 'windows-nt)
-		(progn
-		  (setq doxygen-config-file
-			(replace-regexp-in-string "/" "\\\\" doxygen-config-file ))
-		  )
-	      )
-	    ;; jump to doxygen.cfg's directory to launch it properly
-	    (cd doxygen-config-parent-directory)
-	    (compile (concat "doxygen " doxygen-config-file
-			     " && firefox " "file://" doxygen-html-index-file))
-	    ;; restore the initial directory
-	    (cd saved-default-directory)
-	    ;; ;; also automatically launch a view on the generated documentation
-	    ;; (start-process-shell-command "Browse Doxygen" nil 
-	    ;; 				 (concat "firefox " 
-	    ;; 					 "file://" doxygen-html-index-file))
-	    )
-	  ;; a message in case of error
-	  (message "doxygen.conf not found, no documentation generated !")
-	  )
-    )
-  )
 
 
 ;; *****************************************************************************
@@ -771,6 +724,7 @@ select the source buffer."
             ))
 
 
+
 ;; -----------------------------------------------------------------------------
 ;; Basic setup for ido
 ;; -----------------------------------------------------------------------------
@@ -794,6 +748,7 @@ select the source buffer."
 ;;
 (add-to-list 'load-path "/home/thibauld/Development/wateronmars/wom-experiment")
 (require 'python-django)
+
 
 
 
